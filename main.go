7d76c6e3c4b4692ae336d7de0e3d64eed2ef8673
mike <mike@rh94.n3uc.com>
2025-01-13 12:42:09 -0500

    set imagge to version 1.0.1 and Chart 0.0.4-1

--- main.go
+++ main.go
@@ -1,21 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
+type Config struct {
+	Message string `json:"message"`
+}
+
 var val int
-var msg  string
+var msg string
+var cfg Config
+
+func ReadConfig() {
+	jsonFile, err := os.Open("config.json")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer jsonFile.Close()
+
+	log.Println("Successfully Opened config.json")
+	byteValue, _ := io.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &cfg)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%v, %v\nHello from version 3", msg, val)
 	val++
+	if cfg.Message == "" {
+		// No Config File so lets status an error
+		w.WriteHeader(http.StatusTeapot)
+	}
+	fmt.Fprintf(w, "serv Test program\n<%v> %v, %v", cfg.Message, msg, val)
 }
 
 func main() {
+	ReadConfig()
 	flag.StringVar(&msg, "msg", "Hello World", "message to write out")
 	flag.Parse()
 	http.HandleFunc("/", homePage)
